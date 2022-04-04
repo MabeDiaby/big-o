256b27d0696e8ca00441458867b661d5fc212276
Mabe-Diaby <diabymabe@gmail.com>
2022-04-04 18:49:05 -0400

    big-o

---
+++ script.js
@@ -0,0 +1,17 @@
+// Setting our n
+n = 30000
+
+t0 = new Date().getTime()
+
+// Algo of consideration --------------
+for (let i = 0; i < n; i++){
+
+	for (let j = 0; j < n; j++) {
+		1 + 1
+		// i == j
+	}
+}
+// -----------------------------
+
+t1 = new Date().getTime()
+console.log(t1 - t0);
